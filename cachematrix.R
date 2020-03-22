fab1a9e71b2b905fd6d02c194da507b3587a28d1
brennovmh <61672974+brennovmh@users.noreply.github.com>
2020-03-22 12:59:47 -0300

    Add files via upload
    Programming Assignment 2: Lexical Scoping

---
+++ cachematrix.R
@@ -0,0 +1,36 @@
+## The first function makeCacheMatrix creates a special "matrix", 
+## which is really a list containing a function
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+
+
+## This function computes the inverse of 
+## the special "matrix" returned by makeCacheMatrix
+
+cacheSolve <- function(x, ...) {
+        i <- x$getinverse()
+        if (!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
+}
+
